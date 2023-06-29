1255c220f3c771df8b2786201301b89667832ed3
Laura <lauraelena366@gmail.com>
2023-06-29 18:09:25 +0300

    not gate for 0

---
+++ negat1.py
@@ -0,0 +1,106 @@
+from typing import Union
+
+import pygame
+import numpy as np
+from pygame import Surface, SurfaceType
+from pygame.locals import *
+
+col_about_to_die = (200, 200, 225)
+col_alive = (255, 255, 215)
+col_background = (10, 10, 40)
+col_grid = (30, 30, 60)
+
+def update(surface, cur, sz):
+    nxt = np.zeros((cur.shape[0], cur.shape[1]))
+
+    for r, c in np.ndindex(cur.shape):
+        num_alive = np.sum(cur[r-1:r+2, c-1:c+2]) - cur[r, c]
+
+        if cur[r, c] == 1 and num_alive < 2 or num_alive > 3:
+            col = col_about_to_die
+        elif (cur[r, c] == 1 and 2 <= num_alive <= 3) or (cur[r, c] == 0 and num_alive == 3):
+            nxt[r, c] = 1
+            col = col_alive
+
+        col = col if cur[r, c] == 1 else col_background
+        pygame.draw.rect(surface, col, (c*sz, r*sz, sz-1, sz-1))
+
+    return nxt
+
+def init(dimx, dimy):
+    cells = np.zeros((dimy, dimx))
+    eater = np.array([[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+                        [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+                        [0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0],
+                        [0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,1,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0],
+                        [1,1,0,0,0,0,0,0,0,0,1,0,0,0,0,0,1,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+                        [1,1,0,0,0,0,0,0,0,0,1,0,0,0,1,0,1,1,0,0,0,0,1,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+                        [0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,1,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+                        [0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+                        [0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0], ###ultima originala
+                        [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+                        [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+                        [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+                        [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0],
+                        [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0],
+                        [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]]);
+    pattern = np.array([[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+                        [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+                        [0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0],
+                        [0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,1,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0],
+                        [1,1,0,0,0,0,0,0,0,0,1,0,0,0,0,0,1,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+                        [1,1,0,0,0,0,0,0,0,0,1,0,0,0,1,0,1,1,0,0,0,0,1,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+                        [0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,1,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+                        [0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+                        [0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]]);
+    eater1 = np.array([[1, 1, 0, 0], [1, 0, 0, 0], [0, 1, 1, 1], [0, 0, 0, 1]]);
+
+    pos = (3,3)
+    pos1 = (41,20)
+    pos2 = (60, 73)
+    pattern_rot = np.rot90(eater)
+    cells[pos[0]:pos[0]+pattern.shape[0], pos[1]:pos[1]+pattern.shape[1]] = pattern
+    cells[pos1[0]:pos1[0] + pattern_rot.shape[0], pos1[1]:pos1[1] + pattern_rot.shape[1]] = pattern_rot
+    cells[pos2[0]:pos2[0] + eater1.shape[0], pos2[1]:pos2[1] + eater1.shape[1]] = eater1
+    return cells
+
+def main(dimx, dimy, cellsize):
+    pygame.init()
+    surface: Union[Surface, SurfaceType] = pygame.display.set_mode((dimx * cellsize, dimy * cellsize))
+    pygame.display.set_caption("John Conway's Game of Life")
+
+    font = pygame.font.Font(None, 27)
+
+    cells = init(dimx, dimy)
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                return
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_SPACE:
+                    return
+
+        surface.fill(col_grid)
+        cells = update(surface, cells, cellsize)
+
+        text = font.render("DISTURBER", True, (200, 200, 225))
+        text10 = font.render("OUTPUT: 1", True, (200, 200, 225))
+        text1 = font.render("I", True, (200, 200, 225))
+        text2 = font.render("N", True, (200, 200, 225))
+        text3 = font.render("P", True, (200, 200, 225))
+        text4 = font.render("U", True, (200, 200, 225))
+        text5 = font.render("T", True, (200, 200, 225))
+        surface.blit(text, (10, 10))
+        surface.blit(text10, (500, 460))
+        surface.blit(text1, (84, 350))
+        surface.blit(text2, (80, 370))
+        surface.blit(text3, (80, 390))
+        surface.blit(text4, (80, 410))
+        surface.blit(text5, (80, 430))
+
+        pygame.display.update()
+
+if __name__ == "__main__":
+    main(120, 90, 6)
